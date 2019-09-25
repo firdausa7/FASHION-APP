72147e3210d7875c2f7e31bddfbb55013ad939f1
Paulwamaria <paulwamaria@gmail.com>
2019-09-25 11:32:35 +0300

    Create the post form and comments form

---
+++ app/main/forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from ..models import User, Post, Comment
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+
+
+
+
+class PostForm(FlaskForm):
+    title= StringField('Title', validators = [DataRequired()])
+    content = TextAreaField('Content', validators = [DataRequired()])
+    submit = SubmitField('Post')
+
+
+class CommentForm(FlaskForm)
+
